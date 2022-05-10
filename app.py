d04eff461f7dabba461be381769723b3500dd48a
sivanagasingu <55906882+sivanagasingu@users.noreply.github.com>
2022-05-10 17:07:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!! Prepare for interviews -----Siva you are great!!!!!!!!'
+    return 'Please subscribe, like, and comment on this video, TY!!! Prepare for interviews -----Siva!!!!'
