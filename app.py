26812eea838d72252430c5f9d4bfaf6ecad1ee05
sivanagasingu <55906882+sivanagasingu@users.noreply.github.com>
2022-05-17 14:03:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!! This is GitOPs with ArgoCD Demo from Security Delivery!'
+    return 'Please subscribe, like, and comment on this video, TY!!! This is GitOPs with ArgoCD Demo from Security Delivery!!'
